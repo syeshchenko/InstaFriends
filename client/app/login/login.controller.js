787539eddff6a43363cd9f88d7fb748def724283
Hamabama <yastr.dima@gmail.com>
2016-04-17 16:54:43 -0400

    added simple function to authenticate user at Instagram

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -8,6 +8,17 @@
 
   function LoginController($state) {
     var vm = this;
+
+    vm.login = function() {
+
+      var client_id = "e4ad4e115bf34ef495c72e36600302ef";
+      var scope = "email";
+      var redirect_uri = "https://localhost:5000";
+      var response_type = "token";
+      var url = "https://api.instagram.com/oauth/authorize/?client_id=" +
+      client_id + "&redirect_uri=" + redirect_uri + "&response_type=" + response_type;
+      window.location.replace(url);
+    }
   }
 
 })();
