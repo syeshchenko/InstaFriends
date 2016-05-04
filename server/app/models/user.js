94b2a28e993364190e2b968e517722d7bb95e798
syeshchenko <stanislav.yeshchenko@gmail.com>
2016-05-03 20:13:45 -0400

    add instagram auth, save model to mongo db, redirect back to client

---
+++ server/app/models/user.js
@@ -0,0 +1,14 @@
+// get an instance of mongoose and mongoose.Schema
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// set up a mongoose model and pass it using module.exports
+module.exports = mongoose.model('User', new Schema({
+  instagram: {
+    id: String,
+    token: String,
+    name: String,
+    username: String,
+    profilePicture: String
+  }
+}));
