0645cb6b878a397fdcb5a2651f8d6a1cdcc623fe
syeshchenko <stanislav.yeshchenko@gmail.com>
2016-05-10 21:55:09 -0400

    add ssl certificate, start https with fallback to http

--- server/config.js
+++ server/config.js
@@ -7,7 +7,7 @@ module.exports = {
       'instagram': {
         'clientID'      : '5f28b93eb3be4f609390bee41e778443', // your App ID
         'clientSecret'  : 'ace5603d433f452f9a4af3d6e6fd37ec', // your App Secret
-        'callbackURL'   : 'http://localhost:5000/auth/instagram/callback'
+        'callbackURL'   : 'https://localhost:5000/auth/instagram/callback'
       }
     }
 };
