bdd52e23b1afc0d192df7c48b596947a24bb4cff
Hamabama <yastr.dima@gmail.com>
2016-05-29 20:13:14 -0400

    refactor authorization

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -4,20 +4,11 @@
   angular.module('app.login')
   .controller('LoginController', LoginController);
 
-  LoginController.$inject = ['$state'];
 
-  function LoginController($state) {
+  function LoginController() {
     var vm = this;
 
     vm.login = function() {
-
-      // var client_id = "e4ad4e115bf34ef495c72e36600302ef";
-      // var scope = "email";
-      // var redirect_uri = "https://localhost:5000";
-      // var response_type = "token";
-      // var url = "https://api.instagram.com/oauth/authorize/?client_id=" +
-      // client_id + "&redirect_uri=" + redirect_uri + "&response_type=" + response_type;
-
       window.location.replace("/auth/instagram");
     }
   }
--- client/app/login/login.module.js
+++ client/app/login/login.module.js
@@ -1,5 +1,5 @@
 (function(){
   'use strict';
 
-  angular.module('app.login',[]);
+  angular.module('app.login',['ngCookies']);
 })();
