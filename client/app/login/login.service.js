67acce22ddf10f7a94b403a1174e22f8df11fb74
Hamabama <yastr.dima@gmail.com>
2016-06-26 16:37:51 -0400

    updated logic to keep login on the client during the session and checking server for authorization, refactor

--- client/app/login/login.service.js
+++ client/app/login/login.service.js
@@ -4,26 +4,51 @@
   angular.module('app.login')
   .factory('AuthService', AuthService);
 
-  AuthService.$inject = ['$http'];
+  AuthService.$inject = ['$http', '$rootScope', '$state'];
 
 
-  function AuthService($http) {
+  function AuthService($http, $rootScope, $state) {
+
+    $rootScope.isLoggedIn = false;
 
     return {
-      IsUserLoggedIn: IsUserLoggedIn
+      isLoggedIn: isLoggedIn,
+      setUserLoggedOut: setUserLoggedOut
+    };
+
+    function isLoggedIn() {
+      if ($rootScope.isLoggedIn) return true;
+      else return IsUserLoggedInOnServer().
+      then(setUserLoggedInOnClient);
     }
 
     function setUserLoggedOut() {
-      return $http.get('/api/logout')
+      $rootScope.isLoggedIn = false;
+      $http.get('/api/logout').
+      success(redirectToLogin).
+      error(logoutError);
     }
 
-    function IsUserLoggedIn() {
-      return $http.get('/api/isloggedin')
-      .then(extractAnswer)
+    function redirectToLogin() {
+      $state.go('login');
+    }
+
+    function logoutError(err) {
+      console.log('Couldn\'t logout', err);
+    }
+
+    function IsUserLoggedInOnServer() {
+      return $http.get('/api/isloggedin').
+      then(extractAnswer);
     }
 
     function extractAnswer(res) {
       return res.data.isLoggedIn;
+    }
+
+    function setUserLoggedInOnClient(loggedIn) {
+      if (loggedIn === true) $rootScope.isLoggedIn = true;
+      return loggedIn;
     }
 
   }
