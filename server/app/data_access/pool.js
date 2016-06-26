013eabd8f1e7353b6e7a7309bab3012aa959e57b
syeshchenko <stanislav.yeshchenko@gmail.com>
2016-06-26 12:31:12 -0400

    clean up

--- server/app/data_access/pool.js
+++ server/app/data_access/pool.js
@@ -8,35 +8,37 @@ function getNextCandidate(user, callback) {
       callback(err);
     } else {
       var getNextCandidateQuery = 'SELECT ' +
-         'user.id, user.user_name, user.profile_picture, user.is_active, user.social_media_type_id ' + 
-      'FROM ' + 
-         'users AS user ' +
-      'WHERE ' +
-         ' NOT EXISTS ' +
-            '( SELECT *  ' +
-              'FROM user_follows AS follower ' +
-              'WHERE follower.users_id = user.id ' +
-                'AND follower.shown_users_id = ' + user.id + ' ' + 
-            ') ' +
-      'AND ' +
-         'NOT EXISTS ' +
-            '( SELECT * ' +
-              'FROM user_follows AS followed ' +
-              'WHERE followed.shown_users_id = user.id ' +
-                'AND followed.users_id = ' + user.id + ' ' + 
-            ') ' +
-      'AND ' +
-         'user.id <> ' + user.id + ' ' + 
-      'LIMIT 1';
+        'user.id, user.user_name, user.profile_picture, user.is_active, user.social_media_type_id ' +
+        'FROM ' +
+        'users AS user ' +
+        'WHERE ' +
+        ' NOT EXISTS ' +
+        '( SELECT *  ' +
+        'FROM user_follows AS follower ' +
+        'WHERE follower.users_id = user.id ' +
+        'AND follower.shown_users_id = ' + user.id + ' ' +
+        ') ' +
+        'AND ' +
+        'NOT EXISTS ' +
+        '( SELECT * ' +
+        'FROM user_follows AS followed ' +
+        'WHERE followed.shown_users_id = user.id ' +
+        'AND followed.users_id = ' + user.id + ' ' +
+        ') ' +
+        'AND ' +
+        'user.id <> ' + user.id + ' ' +
+        'LIMIT 1';
 
-      connection.query(getNextCandidateQuery, function(err, result) {
+      connection.query(getNextCandidateQuery, function (err, result) {
+        connection.release();
+
         if (err) {
           callback(err);
         } else {
           if (result.length) {
             callback(null, result[0]);
           } else {
-            callback({message: 'Unable to get next candidate from DB'}, null);
+            callback({ message: 'Unable to get next candidate from DB' });
           }
         }
       });
@@ -49,21 +51,21 @@ function refuseCandidate(params, callback) {
   // get available db connection from the pool
   pool.getConnection(function (err, connection) {
     if (err) {
-      return callback(err);
+      callback(err);
+    } else {
+      var query = 'INSERT into user_follows (users_id, shown_users_id, followed, created_date) values( ' + params.currentUser.id + ', ' + params.refusedUserId + ', ' + 0 + ', ' + 'NOW())'
+
+      connection.query(query, function (err, result) {
+       connection.release();
+
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, result);
+        }
+      });
     }
-
-    var query = 'INSERT into user_follows (users_id, shown_users_id, followed, created_date) values( ' + params.currentUser.id + ', ' + params.refusedUserId + ', ' + 0 + ', ' + 'NOW())'
-
-    connection.query(query, function (err, result) {
-      if (err) {
-        callback(err);
-      } else {
-        callback(null, result);
-      }
-    });
-
   });
-
 };
 
 module.exports.getNextCandidate = getNextCandidate;
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var session    = require('express-session');
 var flash      = require('connect-flash');
 
 var config             = require('./config');
-var route              = require('./routes');
+var router              = require('./routes');
 var passportController = require('./app/controllers/passport');
 var serverConroller    = require('./app/controllers/server');
 
@@ -35,7 +35,7 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 app.use(express.static("../client/"));
 
 // set up routes
-route.setup(apiRoutes, app, passport);
+router.setup(apiRoutes, app, passport);
 app.use('/api', apiRoutes);
 
 // start http/https server
