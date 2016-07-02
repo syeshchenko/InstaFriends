940d3d77a28f543185d76becc127e3adbb7451b6
Hamabama <yastr.dima@gmail.com>
2016-07-01 22:05:29 -0400

    load user profile pic

--- client/app/toolbar/toolbar.controller.js
+++ client/app/toolbar/toolbar.controller.js
@@ -4,14 +4,39 @@
   angular.module('app.toolbar', []).
   controller('ToolbarController', ToolbarController);
 
-  ToolbarController.$inject = ['AuthService'];
+  ToolbarController.$inject = ['AuthService', 'ProfileService'];
 
-  function ToolbarController(AuthService, $state) {
+  function ToolbarController(AuthService, ProfileService) {
     var vm = this;
     vm.isOpen = false;
+    vm.userPicUrl = '';
 
     vm.toggleToolbar = toggleToolbar;
     vm.logout = logout;
+
+    init();
+
+    function init() {
+      getUserPic();
+    }
+
+    function getUserPic() {
+    ProfileService.getProfile().
+    then(extractUserPic).
+    then(displayUserPic);
+    }
+
+    function extractUserPic(data) {
+      return data.data.user.profile_picture;
+    }
+
+    function extractUsername(data) {
+      return data.data.user.user_name;
+    }
+
+    function displayUserPic(url) {
+      vm.userPicUrl = url;
+    }
 
     function toggleToolbar() {
       vm.isOpen = !vm.isOpen;
